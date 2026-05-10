e7ebb8966d68b60516d6e306320c57c3bbeb2968
Isabella <chou.isabella@gmail.com>
2026-05-10 16:01:07 -0500

    XR ray caster to click start button

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,9 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip startSFX;
+    public float volume = 0.5f;
+
    public void StartBtn()
     {
-        SceneManager.LoadScene("StartScreen");
+        SceneManager.LoadScene("SampleScene");
+        
+        audioSource.PlayOneShot(startSFX, volume);
     }
 }
- 
